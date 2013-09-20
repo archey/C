1c83b40d8263388c65e29d46938c91d07b854ea1
Tyler Bennett <tylerb@trix2voip.com>
2013-09-20 15:07:20 -0400

    need to finish ex18.c

---
+++ ex18.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+/** Our old friend die from ex17. */
+void die(const char *message)
+{
+	if(errno) {
+		perror(message
