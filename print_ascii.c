46c208b5091a90ba96460b190eb66de5bab2842e
arch3y <arch3y@archstrike.org>
2018-04-09 21:18:17 -0400

    added code to print all ascii chars

---
+++ print_ascii.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main() {
+    int i = 0; // set var i to type int and set value to 0
+    char ch;
+
+    for (i =0; i < 256; i++) {
+        printf("%c ", ch);
+        ch++;
+    }
+}
