c7fffe1fb72b021e5afe7723c8bcfc2c14970915
arch3y <arch3y@archstrike.org>
2018-04-27 10:48:20 -0400

    coverted code to two funcs, need to test for format string vulnerability

--- factors_of_2.c
+++ factors_of_2.c
@@ -1,18 +1,36 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<math.h>
 
 //TODO
 //Create logic to check if value entered matches base 2 output
 //Create logic to convert binary to decimal
 
+// compile program with gcc -o factors_of_2 factors_of_2.c -lm this links the math header appropriately
+
+void factor_var(int *var){
+    int i, result, base; //defined vars as ints
+    base = *var; // set base to get its value from the pointer var
+    for(i=1; i<=20; i++) {
+      result = pow(base, i); // perform integer to the power of based on base eg, 2^1 as it will loop 1-20
+      printf("%d^%d = %d \n", base, i, result);
+    }
+}
+
 int main() {
-    int base, i, result;
+    int base;
+    //char base[4];
 
     printf("\nEnter an integer: ");
-    scanf("%d", &base);
+    scanf("%d", &base); //vulnerable to buffer overflow, but need to test first
+    //fgets(base,4,stdin);
 
+
+    factor_var(&base);
+    /*
     for(i=1; i<=20; i++) {
         result = pow(base, i);
         printf("%d^%d = %d \n", base, i, result);
     }
+    */
 }
