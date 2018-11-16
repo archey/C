4066a414c710fd865b9173702c8344ad94ee90b2
arch3y <arch3y@arch3y.com>
2018-11-15 22:29:57 -0500

    modified factors_of_2 to provide input from stdin

--- factors_of_2.c
+++ factors_of_2.c
@@ -8,28 +8,24 @@
 
 // compile program with gcc -o factors_of_2 factors_of_2.c -lm this links the math header appropriately
 
-void factor_var(int *var){
-    int i, result; //defined vars as ints
-    for(i=1; i<=20; i++) {
-      result = pow(*var, i); // perform integer to the power of based on base eg, 2^1 as it will loop 1-20
-      printf("%d^%d = %d \n", *var, i, result);
+void factor_var(int *var, int *var2){
+    int num, result; //defined vars as ints
+    for(num=1; num<=*var2; num++) {
+      result = pow(*var, num); // perform integer to the power of based on base eg, 2^1 as it will loop 1-20
+      printf("%d^%d = %d \n", *var, num, result);
     }
 }
 
 int main() {
     int base;
+    int factor;
     //char base[4];
 
     printf("\nEnter an integer: ");
-    scanf("%d", &base); //vulnerable to buffer overflow, but need to test first
-    //fgets(base,4,stdin);
+    scanf("%d", &base); //vulnerable to buffer overflow, but need to test first replace with fgets once bad vuln is written.
+    printf("\nEnter number of times to factor: ");
+    scanf("%d", &factor);
 
-
-    factor_var(&base);
-    /*
-    for(i=1; i<=20; i++) {
-        result = pow(base, i);
-        printf("%d^%d = %d \n", base, i, result);
-    }
-    */
+    // pass pointers of base and factor to the factor_var function
+    factor_var(&base, &factor);
 }
